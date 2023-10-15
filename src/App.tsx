2be08b239c51333f28d2299db6e3139040de2dc8
Sourav Das (Dipta) <dip9623@outlook.com>
2023-10-16 03:39:51 +0600

    added: shadcn dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import { Button } from '@/components/ui/button.tsx';
+import { ThemeProvider } from '@/components/theme-provider.tsx';
+import { ModeToggle } from '@/components/mode-toggle.tsx';
 
 function App() {
   return (
     <>
-      <Button>This is a button</Button>
+      <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <Button>This is a button</Button>
+        <ModeToggle />
+      </ThemeProvider>
     </>
   );
 }
