a53db0472c781749384beebb0c004c2d08f9f8ce
Sourav Das (Dipta) <dip9623@outlook.com>
2023-10-15 21:58:10 +0600

    fix: yarn lint, fix all files

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import {defineConfig} from "vite"
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
-    },
-})
+  },
+});
