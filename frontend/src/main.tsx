61bf511553eab179a595ce3bc603a58933a4ba09
Sourav Das (Dipta) <dip9623@outlook.com>
2023-12-30 02:19:51 +0600

    chore: added notification to remind me to track time

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Timeline from '@/pages/timeline/Timeline.tsx';
-import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { ThemeProvider } from '@/components/theme-provider.tsx';
 import Home from '@/pages/home/home.tsx';
+import { Layout } from './pages/layout/layout';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/timeline',
-    element: <Timeline />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/timeline',
+        element: <Timeline />,
+      },
+    ],
   },
 ]);
 
---
+++ frontend/src/pages/layout/layout.tsx
@@ -0,0 +1,47 @@
+import { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+
+export function Layout() {
+  // TODO: bring in the header in this outlet!
+
+  useEffect(() => {
+    if (!('Notification' in window)) {
+      console.log('Browser does not support notifications');
+    } else {
+      Notification.requestPermission();
+    }
+
+    const audio = new Audio('./public/iphone_sound.mp3');
+    audio.addEventListener('canplaythrough', () => {
+      console.log('notification sound can be played');
+    });
+
+    const interval = setInterval(() => {
+      new Notification(`VI Timer (${new Date().toLocaleString()})`, {
+        body: `Hey! Are you tracking time?`,
+        icon: 'https://www.vkf-renzel.com/out/pictures/generated/product/1/356_356_75/r12044336-01/general-warning-sign-10836-1.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+        dir: 'ltr',
+        requireInteraction: true,
+        data: 'hello',
+      });
+
+      audio.volume = 0.1;
+      audio
+        .play()
+        .then((e) => {
+          console.log('notification should has been played', e);
+        })
+        .catch((e) => {
+          console.error('unable to play notification sound', e);
+        });
+    }, 1000 * 60 * 10); // 10 minutes
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
